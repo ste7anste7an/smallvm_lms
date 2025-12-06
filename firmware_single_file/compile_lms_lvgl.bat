ef387fc5646d92b1a12aa5f04e96d32b470b3c6f
Stefan Op de Beek <ste7an@gmail.com>
2025-12-06 13:57:33 +0100

    allowed pins for ctf version esp32s3

--- firmware_single_file/compile_lms_lvgl.bat
+++ firmware_single_file/compile_lms_lvgl.bat
@@ -7,4 +7,4 @@ pio run -e lms-esp32_st7789_lvgl
 pio run -t buildfs -e lms-esp32_st7789_lvgl
 
 cd firmware_single_file
-call make_all v348
+call make_all v357
--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -950,6 +950,12 @@ void hardwareInit() {
 	#define ANALOG_PINS 20
 	#define TOTAL_PINS 43
 	static const int analogPin[] = {};
+	#if defined(S3_CTF)
+		#define PIN_BUTTON_A 26
+		#define PIN_BUTTON_B 34
+		#define PIN_LED 1
+
+	#else
 	#ifdef LED_BUILTIN
 		#define PIN_LED LED_BUILTIN
 	#elif !defined(PIN_LED)
@@ -962,18 +968,39 @@ void hardwareInit() {
 			#define PIN_BUTTON_A 0
 		#endif
 	#endif
+	#endif
 	// See https://docs.espressif.com/projects/esp-idf/en/latest/esp32s3/api-reference/peripherals/gpio.html
 	// strapping pins 0 (Boot), 3 (JTAG), 45 (VSPI), 46 (LOG)
 	// SPI (26-32); also 33-37 on boards with Octal SPI Flash PSRAM
 	// USB pins: 19 (USB D-), 20 (USB D+)
 	// also possibly: 39-42 (JTAG pins)
+	#if defined(S3_CTF)
+
+	/*Reversed these pins on the board
+
+# sw1 26
+# sw2 34
+# sw3 21
+# sw4 33
+# neopixel 17
+# led1 red 1
+# led2 yellow 2
+# led3 green 3
+# led4 white 4*/
+	static const char reservedPin[TOTAL_PINS] = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+		1, 0, 1, 1, 1, 1, 0, 1, 1, 1,
+		1, 1, 1, 0, 1, 0, 0, 0, 1, 0,
+		0, 0, 0};
+	#else
 	static const char reservedPin[TOTAL_PINS] = {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
 		1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
 		1, 1, 1, 1, 1, 0, 0, 0, 1, 0,
 		0, 0, 0};
-
+	#endif	
 #elif defined(ESP32_C3)
 	#define BOARD_TYPE "ESP32-C3"
 	#define DIGITAL_PINS 22
